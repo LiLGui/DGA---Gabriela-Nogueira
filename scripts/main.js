521ba43f6f5eb6f30d0d3ada024b44fa68846d82
LilGui <rafaelwilliansgi4@gmail.com>
2022-09-21 23:16:00 -0300

    terceiro upp

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,1 @@
-function link() {
-  window.location.href = "https://pay.hotmart.com/error?errorMessage=049";
-};
-const dados = {
-  texto1: "lorem sla oq sla oq",
-}
-const todosJSON = JSON.stringify(todos);
+function link(){window.location.href="https://pay.hotmart.com/error?errorMessage=049"};
